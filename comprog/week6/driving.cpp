f8b46d32aa09e547229b2956a1f21d128a6b5a99
Andrej Kruzliak <kruzland@fel.cvut.cz>
2022-05-25 21:34:32 +0200

    wip

--- comprog/week6/driving.cpp
+++ comprog/week6/driving.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 typedef long long ll;
+bool vb = false;
 
 void show_adj_table(const vector<vector<int>> &adj){
     cout << "--------------\n| ADJ. TABLE |\n--------------\n";
@@ -25,31 +26,45 @@ void print_vector(const vector<int> &vec, const string &txt){
 
 bool first_necessary_condition(int n, const vector<vector<int>> &adj, const vector<int> &indegree){
     /*  
-    i) at most one vertex has out_degree == 1 + indegree
+    i) the first vertex has out_degree == 1 + indegree
     and
-    ii) at most one vertex has in_degree == 1 + outdegree
+    ii) the last vertex has in_degree == 1 + outdegree
     iii) all other vertices: in_degree == out_degree 
     */
-    bool first = false;
-    bool second = false;
-    
+        
     for (int i = 0; i < n; i++){
         // i)
-        if ((int) adj[i].size() == indegree[i] + 1)
-        {
-            if (first) return false;
-            else first = true;
+        if (i == 0){
+            if ((int) adj[i].size() != indegree[i] + 1)
+            {   
+                if(vb){
+                    cout << "i: " << i << "\n";
+                    cout << "1\n";
+                }
+                return false;
+            }
         }
         // ii)
-        else if (indegree[i] == 1 + (int) adj[i].size())
-        {
-            if (second) return false;
-            else second = true;
+        else if (i == n-1){
+            if (indegree[i] != 1 + (int) adj[i].size())
+            {   
+                if(vb){
+                    cout << "i: " << i << "\n";
+                    cout << "2\n";      
+                }
+                
+                return false;
+            }    
         }
         else{
         // iii)
             if (indegree[i] != (int) adj[i].size())
-            {
+            {   
+                if(vb){
+                    cout << "i: " << i << "\n";
+                    cout << "3\n";
+                }
+                
                 return false;
             }
         }
@@ -57,11 +72,26 @@ bool first_necessary_condition(int n, const vector<vector<int>> &adj, const vect
     return true;
 }
 
+
+void flood_dfs(int v, int &visited_cnt, const vector<vector<int>> &adj, vector<bool> &visited){
+    visited[v] = true;
+    visited_cnt++;
+    for (int u : adj[v])
+    {
+        if (!visited[u]){
+            flood_dfs(u, visited_cnt, adj, visited);
+        }
+    }
+}
+
+
 int main(){
     int n, m;
     cin >> n >> m;
     vector<vector<int>> adj(n);
+    vector<bool> visited(n, false);
     vector<int> indegree(n, 0);
+    int visited_cnt = 0;
 
     // read the edges and save them into Adjacency List
     for (int i = 0; i < m; i++){
@@ -74,11 +104,25 @@ int main(){
         // one directional graph
         // adj[second].push_back(first);
     }
-
-    show_adj_table(adj);
-    print_vector(indegree, "Indegree");
-    if (first_necessary_condition(n, adj, indegree)) cout << "True\n";
-    else cout << "False\n";
+    if (vb){
+        show_adj_table(adj);
+        print_vector(indegree, "Indegree");
+    }
+    
+    if (first_necessary_condition(n, adj, indegree)){
+        flood_dfs(0, visited_cnt, adj, visited);
+        // cout << "visited_cnt: " << visited_cnt << " n: " << n << "\n";
+        if (visited_cnt == n){
+            cout << "possible\n";
+            return 0;
+        }
+        else{
+            cout << "impossible\n";
+            return 0;
+        }
+        
+    } 
+    else cout << "impossible\n";
     
     return 0;
 }

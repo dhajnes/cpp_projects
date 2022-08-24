aa3d64511a63d8c903fa07ee18a5f0673cf84ccf
Andrej Kruzliak <kruzland@fel.cvut.cz>
2022-08-24 19:51:18 +0200

    minor changes

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -52,14 +52,14 @@ public:
 // }
 
 // playing board size:
-int board_x = 10;
-int board_y = 20;
-std::vector<std::vector<int>> original_board(board_y, std::vector<int>(board_x, 0));
+int board_c = 10;
+int board_r = 20;
+std::vector<std::vector<int>> original_board(board_r, std::vector<int>(board_c, 0));
 
 void print_board(std::vector<std::vector<int>> &board){
     std::cout << "╔══TETRIS══╗\n";
-    for (int r = 0; r < board_y; r++){
-        for (int c = 0; c < board_x; c++){
+    for (int r = 0; r < board_r; r++){
+        for (int c = 0; c < board_c; c++){
             if (c == 0) std::cout << "║";
             if (board[r][c] == 0){
                 std::cout << "·";
@@ -67,7 +67,7 @@ void print_board(std::vector<std::vector<int>> &board){
             else{
                 std::cout << "█";
             }
-            if (c == board_x-1) std::cout << "║";
+            if (c == board_c-1) std::cout << "║";
         }
         std::cout << "\n";
     }
@@ -97,6 +97,59 @@ std::mt19937 rng(rd());  // Mersenne Twister pseudo-random generator of 32-bit n
 std::uniform_int_distribution<int> tile_uni(0,6);
 std::uniform_int_distribution<int> random_walk(-1,1);
 
+std::pair<int,int> check_valid_move(Tile tile, std::pair<int,int> cursor, int rotation, std::vector<std::vector<int>> &board, bool recursive_flag){
+    /* cursor already moved */
+
+    // auto temp_board = board;
+    // auto temp_tile = tile;  // TODO check if this is a deepcopy, may cause problems if not
+    // If you hold objects by value then the default copy ctor
+    // and assignment operators will do a 'deep copy'.
+    // bcz of ^ I take tile by value, not by reference &, it might work, the calss is not large
+    tile.rot = rotation;
+    std::pair<int, int> correction = {0,0};
+    // TODO do rotation here 
+    auto tile_pieces = tile.shapes[tile.shape];
+    std::pair<int, int> max_deviation = {0,0};
+
+    for (uint i = 0; i < tile_pieces.size(); i++){
+        // check if the vertical step would be correct
+        if (tile_pieces[i][0] + cursor.first - board_r > 0){
+            max_deviation.first = std::max(max_deviation.first, tile_pieces[i][0] + cursor.first - board_r);
+            // TODO if this is not correct, the function should signal that the function must be placed now or on the
+            // next move
+        }  
+        else if(board[tile_pieces[i][0] + cursor.first][tile_pieces[i][1] + cursor.second] == 1){
+            // max_deviation.first = std::max(max_deviation.first, tile_pieces[i][0]+1)
+            auto temp_cursor = cursor;
+            if (recursive_flag){
+                // TODO finish output from the `check_valid_move` to get maximal number of steps we need to move the tile in order to be valid
+                // then choose the smaller move that would satisfy the validity (horizontal vs vertical)
+                for(int j = 0; j < 4; j++){
+                    temp_cursor.first++;
+                    auto disparities = check_valid_move(tile, temp_cursor, rotation, board, false);
+                }
+
+                temp_cursor = cursor;
+                for(int j = 0; j < 4; j++){
+                    temp_cursor.second++;
+                    check_valid_move(tile, temp_cursor, rotation, board, false);
+                }
+            }
+        }
+
+        // check if the horisontal step would be correct
+        if (tile_pieces[i][1] + cursor.second - board_c > 0){ 
+                    check_valid_move(tile, temp_cursor, rotation, board, false);
+            max_deviation.second = std::max(max_deviation.second, tile_pieces[i][1] + cursor.second - board_c);
+
+        }
+
+        else if ()
+    }
+
+    
+}
+
 
 
 int main(){
@@ -115,14 +168,14 @@ int main(){
 
     bool game_on = true;
     int rand_piece_id;
-    std::vector<int> cursor;
+    std::pair<int,int> cursor;
     while(game_on){ // outer loop, new choice
         cursor = {0, 4};
         rand_piece_id = tile_uni(rng);
 
         while(1){
             Tile new_tile(0,rand_piece_id);
-            int movement_input = random_walk(rng);
+            int movement_increment = random_walk(rng);
             // cursor // TODO add maxing and mining of cursor values so its not possible to get out of bounds
             // possibly use a wrapper function for this that checks that the whole piece wouldnt be out of bounds
 

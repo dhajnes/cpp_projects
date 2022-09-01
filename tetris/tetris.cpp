d0647fbd149f0950e74bdc39ad8169ee3725b9d9
Andrej Kruzliak <kruzland@fel.cvut.cz>
2022-09-02 00:37:42 +0200

    installed  instead of using raw terminal, arrows work

--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
+#include <iostream>
 #include <chrono>
 #include <thread>
 #include <random>
+#include <ncurses.h>
 #include <Eigen/Dense>
+
+#include <stdio.h>
+
+// COMPILE WITH: g++ tetris.cpp -o tetris -lncursesw
  
 typedef long long ll;
 
@@ -63,22 +69,44 @@ int board_c = 10;
 int board_r = 20;
 std::vector<std::vector<int>> original_board(board_r, std::vector<int>(board_c, 0));
 
-void print_board(std::vector<std::vector<int>> &board){
-    std::cout << "╔══TETRIS══╗\n";
+std::string print_board(std::vector<std::vector<int>> &board){
+    std::string to_display;
+    to_display += "+--TETRIS--+\n";
     for (int r = 0; r < board_r; r++){
         for (int c = 0; c < board_c; c++){
-            if (c == 0) std::cout << "║";
+            if (c == 0) to_display += "|";
             if (board[r][c] == 0){
-                std::cout << "·";
+                to_display += ".";
             }
             else{
-                std::cout << "█";
+                to_display += "X";
             }
-            if (c == board_c-1) std::cout << "║";
+            if (c == board_c-1) to_display += "|";
         }
-        std::cout << "\n";
+        to_display += "\n";
     }
-    std::cout << "╚══════════╝\n";
+    to_display += "+----------+\n";
+    return to_display;
+}
+
+std::string print_board_alt(std::vector<std::vector<int>> &board){
+    std::string to_display;
+    to_display += "╔══TETRIS══╗\n";
+    for (int r = 0; r < board_r; r++){
+        for (int c = 0; c < board_c; c++){
+            if (c == 0) to_display += "║";
+            if (board[r][c] == 0){
+                to_display += "·";
+            }
+            else{
+                to_display += "█";
+            }
+            if (c == board_c-1) to_display += "║";
+        }
+        to_display += "\n";
+    }
+    to_display += "╚══════════╝\n";
+    return to_display;
 }
 
 std::vector<std::vector<int>> place_tile(std::vector<std::vector<int>> &board, Tile &tile, std::vector<int> cursor){
@@ -108,7 +136,7 @@ void do_rotation(Eigen::MatrixXd &tile_pieces, int rotation){
     Eigen::MatrixXd rot(2,2);
     rot << 0, -1, 1, 0;  // rot = 90 deg
     for (int i = 0; i < rotation; i++){ // n of rotations
-        tile_pieces = rot * tile_pieces;
+        tile_pieces = rot * tile_pieces;  // TODO this should be zeroed out so there are no negative numbers
     }
 }
 
@@ -141,32 +169,91 @@ int main(){
     
     
     auto board = original_board;
-
-    
-    
-    
-
-    // here a while loop until the block has place to fall
-    // i)  has a place to fall, then continnue for the block to fall
-    // ii) if not possible for the block fall anymore, place it
-    //      - this means that `board = trace_board` and begin the loop again, reset cursor and choose a random tile 
-
     bool game_on = true;
     int rand_piece_id;
     std::pair<int,int> cursor;
+    bool bKey[4];
+
+    // ncurses initialization for catching the arrows
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    clear();
+    int row, col;	
+    int rot = 0;
+    getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+    
+    int ch;
+    printw("[ TETRIS ]");
+    printw("Press E to Exit\n");
+    wait(1000);
+
+
     while(game_on){ // outer loop, new choice
+
         cursor = {0, 4};
         rand_piece_id = tile_uni(rng);
 
+        // one tetris-piece episode
         while(1){
-            Tile new_tile(0,rand_piece_id);
-            int movement_increment = random_walk(rng);
-            // cursor // TODO add maxing and mining of cursor values so its not possible to get out of bounds
-            // possibly use a wrapper function for this that checks that the whole piece wouldnt be out of bounds
+            
+            // GAME TIMING ==========================
+            wait(50);
+            timeout(1);
+
+            // GAME INPUT  ==========================
+
+            ch = getch();
+            
+            switch(ch)
+            {
+            case KEY_UP:
+                // Change rotation
+                rot++;
+                rot = rot % 4;
+                break;
+            case KEY_DOWN:
+                // TODO fall faster
+                break;
+            case KEY_LEFT:
+                cursor.second--;
+                cursor.second = std::max(cursor.second, 0);
+                
+                break;
+            case KEY_RIGHT:
+                // printw("\nRight Arrow");
+                cursor.second++;
+                cursor.second = std::min(cursor.second, board_c-1);
+                break;
+            case 'E':
+                game_on = false;
+                break;
+            }
+
+            // GAME LOGIC  ==========================
+
+
+            
+
+            // RENDER OUT  ==========================
+            printw(print_board(board).c_str());
+            refresh();
+            
+            clear();
+            
+
+            // while(1){
+            //     Tile new_tile(0,rand_piece_id);
+            //     int movement_increment = random_walk(rng);
+            //     // possibly use a wrapper function for this that checks that the whole piece wouldnt be out of bounds
+
+            // }
 
         }
-
-    }
+        }
+        
+    endwin();
   
 
   // TODO test do_rotation function also correct representation of all pieces in the matrix notation
